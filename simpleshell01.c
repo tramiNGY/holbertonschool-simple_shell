37b90d106ffc7bc24a0aa274fbc264b60a0cf903
tramiNGY <ngytrami@gmail.com>
2025-01-09 11:05:17 +0100

    Added environ and replaced it in execve to handle PATH

--- simpleshell01.c
+++ simpleshell01.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 /**
  * readline - read a line from stdin
  * Return: buffer or NULL
@@ -20,6 +21,37 @@ char *readline(void)
 	buffer[strcspn(buffer, "\n")] = 0;
 	return (buffer);
 }
+
+/**
+ * _strtok - splits command in arguments
+ * @command: string  to be split with space delimiter
+ * Return: pointer to an array of pointers to arguments
+ */
+char **_strtok(char *command)
+{
+	int i;
+	char *token;
+	char **args;
+
+	args = malloc(sizeof(char *) * 64);
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(1);
+	}
+
+	token = strtok(command, " ");
+	i = 0;
+	while (token != NULL)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	return (args);
+}
+
 /**
  * main - execute command or print error message
  * @argc: number of arguments
@@ -30,39 +62,38 @@ int main(int argc, char **argv)
 {
 	int status;
 	char *command;
+	extern char **environ;
+	char **args;
 	pid_t child_pid;
-	char *args[2], *token;
-	(void) argc;
-	(void) argv;
+	(void) argc, (void) argv;
+
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
+			printf(":) ");
 		command = readline();
 		if (command == NULL)
 			break;
-		token = strtok(command, " ");
-		while (token != NULL)
+		if (strlen(command) == 0)
+			continue;
+		args = _strtok(command);
+		child_pid = fork();
+		if (child_pid == 0)
 		{
-			args[0] = token;
-			args[1] = NULL;
-			child_pid = fork();
-			if (child_pid == 0)
+			if (execve(args[0], args, environ) == -1)
 			{
-				if (execve(args[0], args, NULL) == -1)
-				{
-					perror("./shell");
-					free(command);
-					exit(1);
-				}
+				perror("./shell");
+				free(command);
+				free(args);
+				exit(1);
 			}
-			else if (child_pid < 0)
-				perror("fork");
-			else
-				waitpid(child_pid, &status, 0);
-			token = strtok(NULL, " ");
 		}
+		else if (child_pid < 0)
+			perror("fork");
+		else
+			waitpid(child_pid, &status, 0);
 		free(command);
+		free(args);
 	}
 	return (0);
 }
