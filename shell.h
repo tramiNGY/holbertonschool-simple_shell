924a41b5a4264967faf770dc11db231a3e00598b
tramiNGY <ngytrami@gmail.com>
2025-01-09 15:36:28 +0100

    Changed method without _getenv function with with a linked list containing environment directory

--- shell.h
+++ shell.h
@@ -1,5 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+typedef struct list_T
+{
+	char *str;
+	unsigned int len;
+	struct list_T *next;
+} list_t;
 
 #endif
--- simpleshell01.c
+++ simpleshell01.c
@@ -7,6 +7,83 @@
 #include "shell.h"
 
 /**
+ * add_node_end - add a new node to the end of the list
+ * @head: double pointer to the start of the list
+ * @str: string to add
+ * Return: the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+	list_t *tempnode;
+	unsigned int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->str = strdup(str);
+	if (newnode->str == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->len = length;
+	newnode->next = NULL;
+	if (*head == NULL)
+		*head = newnode;
+	else
+	{
+		tempnode = *head;
+		while (tempnode->next != NULL)
+			tempnode = tempnode->next;
+		tempnode->next = newnode;
+	}
+	return (newnode);
+}
+
+/**
+ * get_full_path - search for an executable in directories listed in PATH
+ * @command: the command to search for
+ * @head: list of directories
+ * Return: path to the executable or NULL if not found
+ */
+char *get_full_path(char *command, list_t *head)
+{
+	char *fullpath;
+	struct stat buffer;
+	list_t *temp = head;
+
+	if (command[0] == '/')
+	{
+		if (stat(command, &buffer) == 0)
+			return (command);
+	}
+
+	while (temp != NULL)
+	{
+		fullpath = malloc(strlen(temp->str) + strlen(command) + 2);
+		if (!fullpath)
+		{
+			perror("malloc");
+			exit(1);
+		}
+		sprintf(fullpath, "%s/%s", temp->str, command);
+
+		if (stat(fullpath, &buffer) == 0)
+		{
+			return (fullpath);
+		}
+
+		free(fullpath);
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
+
+/**
  * readline - read a line from stdin
  * Return: buffer or NULL
  */
@@ -23,87 +100,74 @@ char *readline(void)
 	buffer[strcspn(buffer, "\n")] = 0;
 	return (buffer);
 }
+
 /**
- * _getenv - replica of getenv function which get an environment variable
- * @name: name of the environment variable to get
- * @env: environment variable
- * Return: pointer to the environment variable
+ * _strtok - splits command in arguments
+ * @command: string to be split with space delimiter
+ * Return: pointer to an array of pointers to arguments
  */
-char *_getenv(const char *name, char **env)
+char **_strtok(char *command)
 {
-	int i, j;
-	int status;
+	int i;
+	char *token;
+	char **args;
 
-	for (i = 0; env[i] != NULL; i++)
+	args = malloc(sizeof(char *) * 64);
+	if (args == NULL)
 	{
-		status = 1;
-		for (j = 0; env[i][j] != '='; j++)
-		{
-			if (name[j] != env[i][j])
-			{
-				status = 0;
-				break;
-			}
-		}
-		if (status)
-		{
-			return (&env[i][j + 1]);
-		}
+		perror("malloc");
+		exit(1);
 	}
-	return (NULL);
+
+	token = strtok(command, " ");
+	i = 0;
+	while (token != NULL)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	return (args);
 }
 
-/**
- * get_full_path - get absolute path of command
- * @command: command alone without full path
- * @env: environment variable
- * Return: returns absolute path to command
-*/
-
-char *get_full_path(char *command, char **env)
-{
-	char *path = _getenv("PATH", env);
-	char *dir = strtok(path, ":");
-	char *full_path = NULL;
-	struct stat buffer;
-
-	if (command[0] == '/')
-	{
-		full_path = strdup(command);
-		return (full_path);
-	}
-	while (dir != NULL)
-	{
-		full_path = malloc(strlen(dir) + strlen(command) + 2);
-		if (full_path == NULL)
-		{
-			perror("malloc");
-			return (NULL);
-		}
-		sprintf(full_path, "%s/%s", dir, command);
-		if (stat(full_path, &buffer) == 0)
-		{
-			return (full_path);
-		}
-		free(full_path);
-		full_path = NULL;
-		dir = strtok(NULL, ":");
-	}
-	return (NULL);
-}
 /**
  * main - execute command or print error message
  * @argc: number of arguments
  * @argv: list of arguments
- * @env: environment variable
  * Return: 0 if success
  */
 int main(int argc, char **argv, char **env)
 {
 	int status, i;
-	char *command, *token, *args[64];
+	char *command;
+	char **args;
+	char *exe_path;
 	pid_t child_pid;
-	(void) argc, (void) argv;
+	list_t *head = NULL, *temp, *next;
+	char *path_var = NULL, *copy, *token;
+	(void)argc, (void)argv;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		if (strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path_var = &env[i][5];
+			break;
+		}
+	}
+	if (path_var)
+	{
+		copy = strdup(path_var);
+		token = strtok(copy, ":");
+		while (token != NULL)
+		{
+			add_node_end(&head, token);
+			token = strtok(NULL, ":");
+		}
+		free(copy);
+	}
+
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
@@ -113,31 +177,43 @@ int main(int argc, char **argv, char **env)
 			break;
 		if (strlen(command) == 0)
 			continue;
-		i = 0;
-		token = strtok(command, " ");
-		while (token != NULL)
+
+		args = _strtok(command);
+		exe_path = get_full_path(args[0], head);
+		if (exe_path != NULL)
 		{
-			args[i] = token;
-			i++;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
-		if (args[0][0] != '/')
-			args[0] = get_full_path(args[0], env);
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			if (execve(args[0], args, NULL) == -1)
+			child_pid = fork();
+			if (child_pid == 0)
 			{
-				perror("./shell");
-				free(command);
-				exit(1);
+				if (execve(exe_path, args, env) == -1)
+				{
+					perror("./shell");
+					free(command);
+					free(args);
+					exit(1);
+				}
 			}
+			else if (child_pid < 0)
+				perror("fork");
+			else
+				waitpid(child_pid, &status, 0);
 		}
-		else if (child_pid < 0)
-			perror("fork");
 		else
-			waitpid(child_pid, &status, 0);
+		{
+			perror("Command not found");
+		}
 		free(command);
-	} return (0);
+		free(args);
+	}
+
+	temp = head;
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = next;
+	}
+
+	return (0);
 }
